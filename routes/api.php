942e972d52720b28d2292d0758aabbbf9d1a428c
sithell <thesithell@gmail.com>
2021-02-20 19:36:01 +0300

    Add food

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,4 @@ Route::get('/day/{date}', [\App\Http\Controllers\DayController::class, 'show']);
 Route::post('/water', [\App\Http\Controllers\DayController::class, 'water']);
 Route::post('/drug', [\App\Http\Controllers\DayController::class, 'drug']);
 Route::post('/note', [\App\Http\Controllers\NoteController::class, 'create']);
+Route::get('/food/{name}', [\App\Http\Controllers\FoodController::class, 'show']);
